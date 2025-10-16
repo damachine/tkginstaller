4226cf2bdcbf5eae942157d8c363369d66822bc8
damachine <christkue79@gmail.com>
2025-10-16 13:38:52 +0200

    bump version to 0.13.0
    - Fix: update sha256sum

--- tkginstaller.sh
+++ tkginstaller.sh
@@ -50,7 +50,7 @@
 # shellcheck disable=SC2218
 
 # TKG-Installer VERSION
-readonly _TKG_INSTALLER_VERSION="v0.12.9"
+readonly _TKG_INSTALLER_VERSION="v0.13.0"
 
 # Lock file to prevent concurrent execution
 readonly _LOCK_FILE="/tmp/tkginstaller.lock"
