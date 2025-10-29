43e4087f305850b8b0d95d910c69deb7e1478b73
damachine <christkue79@gmail.com>
2025-10-29 17:08:51 +0100

    bump: version to v0.23.2
    - fix small tui and typo thanks @olivetti80 for his eagle eyes

--- tkginstaller.sh
+++ tkginstaller.sh
@@ -57,7 +57,7 @@
 # shellcheck disable=SC2218 # Allow usage of printf with variable format strings
 
 # TKG-Installer VERSION definition
-export _tkg_version="v0.23.1"
+export _tkg_version="v0.23.2"
 
 # Lock file to prevent concurrent execution of the script
 export _lock_file="/tmp/tkginstaller.lock"
