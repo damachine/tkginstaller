4199278ad16b99ba0e2fda9518ee0a5228b3a7ca
damachine <christkue79@gmail.com>
2025-10-29 21:41:49 +0100

    bump version to 0.23.4
    - user information about configuration files improved

--- tkginstaller.sh
+++ tkginstaller.sh
@@ -57,7 +57,7 @@
 # shellcheck disable=SC2218 # Allow usage of printf with variable format strings
 
 # TKG-Installer VERSION definition
-export _tkg_version="v0.23.3"
+export _tkg_version="v0.23.4"
 
 # Lock file to prevent concurrent execution of the script
 export _lock_file="/tmp/tkginstaller.lock"
@@ -249,14 +249,13 @@ __msg_pkg() {
     __msg_plain " A wide range of options are available!"
     __msg_plain " Thanks to their flexible configuration and powerful settings, TKG packages"
     __msg_plain " can be precisely tailored to different systems and personal preferences.${_break}"
-    __msg_plain " The${_gray} customization.cfg${_reset} files can be set up using one of two short methods:"
-    __msg_plain "  1) ${_gray} tkginstaller -> Config -> ${_pkg_name,,}${_reset} (interactive menu)${_break}"
+    __msg_plain " The${_gray} customization.cfg${_reset} files can be set up using one of the two short methods listed:"
+    __msg_plain "  1) ${_gray} tkginstaller -> Config -> ${_pkg_name,,}${_reset} (interactive menu)"
     __msg_plain "  2) ${_gray} tkginstaller ${_pkg_name,,} config${_break}${_reset} (direct command)${_break}"
     __msg_plain " The file(s) are saved in the ${_gray} ~/.config/frogminer/${_reset} directory according to the standard specifications."
-    __msg_plain " Once the setup is complete, the configs are immediately ready to be edited and compared"
-    __msg_plain " The tool offers the option to make the adjustments in your preferred text editor."
-    __msg_plain " ${_uline_on}Please make sure to adjust the settings correctly.${_uline_off}"
-    __msg_plain " Location: ${_gray}${_config_url}${_reset}"
+    __msg_plain " Now the files can be adjusted and compared using one of the above-mentioned steps."
+    __msg_plain " ${_uline_on}Please make sure to adjust the settings correctly!${_uline_off}"
+    __msg_plain " See: ${_gray}${_config_url}${_reset}"
 }
 
 # Check for root execution and warn the user (if running as root)
