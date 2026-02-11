5f79c51ddda0e2e4d1661f672f9c07b293f47868
damachine <damachin3@proton.me>
2026-02-11 04:15:57 +0100

    VERSION: bump to 0.30.3

--- install.sh
+++ install.sh
@@ -3,43 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 # shellcheck disable=SC2218 # Allow usage of printf with variable format strings
 
-# -----------------------------------------------------------------------------
-# author : damachine (damachin3 at proton dot me)
-# website: https://github.com/damachine
-#          https://github.com/damachine/tkginstaller
-# -----------------------------------------------------------------------------
-# MIT License
-#
-# Copyright (c) 2025 damachine
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-# -----------------------------------------------------------------------------
-# INFO:
-#   Installation script for TKG-Installer
-# DESCRIPTION:
-#   This script automates the installation of TKG-Installer with integrity verification.
-#   It downloads the latest version from the official repository, verifies the checksum,
-#   and sets up a shell alias for easy access.
-# USAGE:
-#   Simply run this script using cURL:
-#   curl -fsSL https://raw.githubusercontent.com/damachine/tkginstaller/master/install.sh | bash
-# -----------------------------------------------------------------------------
+# Maintainer: damachin3 (damachine3 at proton dot me)
+# website: https://github.com/damachine/tkginstaller
 
 # Initialize color and style (copied from tkginstaller)
 __init_style() {
